f3312db2ad1a28f77f576549d2b854fc0b6d28be
Allison Homem <mrs.allisonhomem@outlook.com>
2021-10-14 22:38:40 -0500

    more bug squashing

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -13,12 +13,12 @@ const restricted = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    next({status: 401, message: 'token required'})
+    res.status(401).json({message: "token required"})
   }
   else {
     jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
       if (err) {
-        next({status: 401, message: 'token invalid'})
+        res.status(401).json({message: "token invalid"})
       }
       else {
         req.decodedToken = decodedToken;
